bb05bbf7bea0c82edae04c5e920ca6a344473b0c
NotStrixx <miguelfandrade2005@gmail.com>
2024-08-08 14:36:58 -0700

    - Fixed About us Funtionality - Small changes in index for better readablity -

--- EventPlanner/Controllers/HomeController.cs
+++ EventPlanner/Controllers/HomeController.cs
@@ -23,6 +23,10 @@ namespace EventPlanner.Controllers
             return View();
         }
 
+        public IActionResult AboutUs()
+        {
+            return View();
+        }
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
